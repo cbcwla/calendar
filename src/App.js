f138952d3d2144306944791f465fb77b4be3eabb
Jennifer Chen <shinyi.chen@gmail.com>
2023-06-07 22:16:41 -0700

    Install and setup Chakra-UI and fullCalendar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import events from './events.js';
+import "./App.css";
+import events from "./events.js";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <ul>{
-        events.map( (event, index) =>
-          <li key={ event.title } style={{padding: '2px', margin: '2px', backgroundColor:  index % 2 ? 'white' : 'lightGray' }}>
-            <p>{ event.title }</p>
-            <p>{ Object.values(event.tags).map( (tag) => `#${tag}`).join(', ') }</p>
-            <p>{ event.start === event.end ? event.start : `from ${event.start} to ${event.end}` } </p>
-            <p>{ event.groups && event.groups.join(', ') }</p>
-            <p>{ event.details && `details in html of ${ event.details.length } chars`}</p>
-          </li>
-        )
-      }</ul>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <ul>
+          {events.map((event, index) => (
+            <li
+              key={event.title}
+              style={{
+                padding: "2px",
+                margin: "2px",
+                backgroundColor: index % 2 ? "white" : "lightGray",
+              }}
+            >
+              <p>{event.title}</p>
+              <p>
+                {Object.values(event.tags)
+                  .map((tag) => `#${tag}`)
+                  .join(", ")}
+              </p>
+              <p>
+                {event.start === event.end
+                  ? event.start
+                  : `from ${event.start} to ${event.end}`}{" "}
+              </p>
+              <p>{event.groups && event.groups.join(", ")}</p>
+              <p>
+                {event.details &&
+                  `details in html of ${event.details.length} chars`}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </ChakraProvider>
   );
 }
 
